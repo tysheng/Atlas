2695114ed21e88ac4da5f308c67f62e46cdc62a3
shengtianyang <353491983@qq.com>
2016-02-15 21:53:39 +0800

    init

---
+++ app/src/main/java/shengtianyang/atlas/app/AtlasApp.java
@@ -0,0 +1,21 @@
+package shengtianyang.atlas.app;
+
+import android.app.Application;
+
+import com.android.volley.RequestQueue;
+import com.android.volley.toolbox.Volley;
+import com.facebook.drawee.backends.pipeline.Fresco;
+
+public class AtlasApp extends Application {
+public static RequestQueue requestQueue;
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        Fresco.initialize(getApplicationContext());
+        requestQueue = Volley.newRequestQueue(getApplicationContext());
+    }
+    public static RequestQueue getRequestQueue(){
+        return requestQueue;
+    }
+}
