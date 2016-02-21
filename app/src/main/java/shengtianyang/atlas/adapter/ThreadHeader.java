1f607708f4694a63dddd0113b3c1eca9ac485684
tysheng <353491983@qq.com>
2016-02-21 17:23:55 +0800

    1.add

---
+++ app/src/main/java/shengtianyang/atlas/adapter/ThreadHeader.java
@@ -0,0 +1,64 @@
+package shengtianyang.atlas.adapter;
+
+/**
+ * Created by shengtianyang on 16/2/21.
+ */
+public class ThreadHeader {
+    public ThreadHeader() {
+    }
+    private String draweeTopic;
+    private String tvTopicAuthor;
+    private String tvTopicNode;
+    private String tvTopicTime;
+    private String tvTopicContent;
+    private String tvTopicTitle;
+
+    public String getDraweeTopic() {
+        return draweeTopic;
+    }
+
+    public void setDraweeTopic(String draweeTopic) {
+        this.draweeTopic = draweeTopic;
+    }
+
+    public String getTvTopicAuthor() {
+        return tvTopicAuthor;
+    }
+
+    public void setTvTopicAuthor(String tvTopicAuthor) {
+        this.tvTopicAuthor = tvTopicAuthor;
+    }
+
+    public String getTvTopicNode() {
+        return tvTopicNode;
+    }
+
+    public void setTvTopicNode(String tvTopicNode) {
+        this.tvTopicNode = tvTopicNode;
+    }
+
+    public String getTvTopicTime() {
+        return tvTopicTime;
+    }
+
+    public void setTvTopicTime(String tvTopicTime) {
+        this.tvTopicTime = tvTopicTime;
+    }
+
+    public String getTvTopicContent() {
+        return tvTopicContent;
+    }
+
+    public void setTvTopicContent(String tvTopicContent) {
+        this.tvTopicContent = tvTopicContent;
+    }
+
+    public String getTvTopicTitle() {
+        return tvTopicTitle;
+    }
+
+    public void setTvTopicTitle(String tvTopicTitle) {
+        this.tvTopicTitle = tvTopicTitle;
+    }
+
+}
---
+++ app/src/main/java/shengtianyang/atlas/fragment/SettingFragment.java
@@ -0,0 +1,34 @@
+package shengtianyang.atlas.fragment;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import butterknife.ButterKnife;
+import shengtianyang.atlas.R;
+
+/**
+ * Created by shengtianyang on 16/2/1.
+ */
+public class SettingFragment extends Fragment {
+
+
+    public SettingFragment() {
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_setting, container, false);
+        ButterKnife.bind(this, view);
+
+        return view;
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        ButterKnife.unbind(this);
+    }
+}
